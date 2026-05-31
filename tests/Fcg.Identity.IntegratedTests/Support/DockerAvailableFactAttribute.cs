a503db3000c24f6143870f789aa4aa955eced6ce
flaviojcf <flaviojcostafilho@gmail.com>
2026-05-30 22:27:15 -0300

    chore: Set as test project to sonar

--- tests/Fcg.Identity.IntegratedTests/Support/DockerAvailableFactAttribute.cs
+++ tests/Fcg.Identity.IntegratedTests/Support/DockerAvailableFactAttribute.cs
@@ -1,11 +1,15 @@
+using System.Diagnostics;
+
 namespace Fcg.Identity.IntegratedTests.Support;
 
 [AttributeUsage(AttributeTargets.Method)]
 public sealed class DockerAvailableFactAttribute : FactAttribute
 {
+    private static readonly Lazy<bool> DockerAvailable = new(IsDockerAvailable);
+
     public DockerAvailableFactAttribute()
     {
-        if (!IsDockerAvailable())
+        if (!DockerAvailable.Value)
         {
             Skip = "Docker is required to run this Testcontainers integration test.";
         }
@@ -28,6 +32,38 @@ public sealed class DockerAvailableFactAttribute : FactAttribute
             return true;
         }
 
-        return !OperatingSystem.IsWindows() && File.Exists("/var/run/docker.sock");
+        if (!OperatingSystem.IsWindows() && File.Exists("/var/run/docker.sock"))
+        {
+            return true;
+        }
+
+        return DockerCliIsAvailable();
+    }
+
+    private static bool DockerCliIsAvailable()
+    {
+        try
+        {
+            using var process = Process.Start(new ProcessStartInfo
+            {
+                FileName = "docker",
+                Arguments = "version --format {{.Server.Version}}",
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            });
+
+            if (process is null)
+            {
+                return false;
+            }
+
+            return process.WaitForExit(milliseconds: 3000) && process.ExitCode == 0;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
